c29c925c7844b6ba8375993d60de3609c7409a80
jigisha <jigishagachuru@336.com>
2025-10-24 12:46:43 +0300

    few updates

--- src/Shop.tsx
+++ src/Shop.tsx
@@ -461,7 +461,10 @@ const products = [
         name: "Silk Scarf",
         category: "fashion",
         price: 29.99,
-        originalPrice: 39.99,
+    originalPrice: 39.99,
+    image: "https://images.unsplash.com/photo-1601762603332-fd61e28b698a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+  }
+];
 
 interface CartItem {
   id: number;
