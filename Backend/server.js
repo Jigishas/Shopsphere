1c115616c32d4c8215b6f7f265ab248c279810d8
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-06 22:17:02 +0300

    few updates

--- Backend/server.js
+++ Backend/server.js
@@ -37,6 +37,19 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 //app.use('/', router);
+
+// Add CORS headers to all responses
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, Access-Control-Request-Method, Access-Control-Request-Headers');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+});
 // Connect to MongoDB
 const connectDB = async () => {
   try {
