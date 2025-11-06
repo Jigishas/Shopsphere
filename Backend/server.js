4685e3fd610d3c4ea93b23e1d65505b8a0f6f18c
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-06 22:51:33 +0300

    few updates

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5175'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: [
