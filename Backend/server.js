0804405da2d90201f3496b4c861597acc1f7cc13
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-06 22:01:56 +0300

    few updates

--- Backend/server.js
+++ Backend/server.js
@@ -13,18 +13,18 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
- app.use(cors({
-   origin: 'http://localhost:5173',
-   credentials: true,
-   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-   allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'X-Requested-With',
-    'Accept',
-    'Origin',
-    'Access-Control-Request-Method',
-    'Access-Control-Request-Headers'
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [
+   'Content-Type',
+   'Authorization',
+   'X-Requested-With',
+   'Accept',
+   'Origin',
+   'Access-Control-Request-Method',
+   'Access-Control-Request-Headers'
   ],
   preflightContinue: false,
   optionsSuccessStatus: 204
@@ -64,7 +64,6 @@ app.options('/api/signup', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin, Access-Control-Request-Method, Access-Control-Request-Headers');
-  res.header('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(204);
 });
 
