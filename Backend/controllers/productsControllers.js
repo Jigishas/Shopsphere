6b88d62651ba42503fa4ccf3db541995ed244d12
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-07 19:50:43 +0300

    few updates

--- Backend/controllers/productsControllers.js
+++ Backend/controllers/productsControllers.js
@@ -12,4 +12,17 @@ const getAllProducts = async (req, res) => {
     res.status(500).json({ message: 'Error fetching products', error: error.message });
   } 
 };
+// GET single product by ID
+const getProductsById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching product', error: error.message });
+  }
+};
 module.exports = { getAllProducts };
