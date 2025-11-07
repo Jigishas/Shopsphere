171a733288fb494666ab0d23d5a0c0d2d78c69ac
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-07 19:51:52 +0300

    few updates

--- Backend/Routers/productRouter.js
+++ Backend/Routers/productRouter.js
@@ -4,5 +4,6 @@ const { getAllProducts } = require('../controllers/productsControllers');
 
 // Route to get all products
 router.get('/api/products', getAllProducts);
+router.get('/api/products/:id', getProductsById);
 
 module.exports = router;
