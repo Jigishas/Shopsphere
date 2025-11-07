b0efb6a3f1876f38aec87cd5db7241d90c2f8beb
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-07 21:14:57 +0300

    few updates

--- Backend/controllers/productsControllers.js
+++ Backend/controllers/productsControllers.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const Products = require('../model/products');
 
 // GET all products
 const getAllProducts = async (req, res) => {
   try {
     const products = await Products.find();
     console.log(products);
-    res.json(products);
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching products', error: error.message });
   } 
