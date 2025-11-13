9129b01d255a611050a2d2f111770d4e811a66b8
Jigisha336 <jigishagachuru336@gmail.com>
2025-11-13 19:23:32 +0300

    first commit

--- Backend/Routers/productRouter.js
+++ Backend/Routers/productRouter.js
@@ -6,7 +6,7 @@ router.get ('/api/products', productsControllers.getAllProducts);
 router.get('/api/products/:id', productsControllers.getProductsById);
 router.post('/api/products', productsControllers.postProduct);
 router.put('/api/products/:id', productsControllers.putProduct);
-router.delete('/api/products/:id', deleteproduct);
+router.delete('/api/products/:id', productsControllers.deleteproduct);
 
 
 module.exports = router;
