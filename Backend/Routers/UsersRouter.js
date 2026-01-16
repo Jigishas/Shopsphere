c5ef7cd1064042f97ec63cb64eee8594edff669e
Jigisha336 <jigishagachuru336@gmail.com>
2026-01-16 16:28:00 +0300

    few updates

--- Backend/Routers/UsersRouter.js
+++ Backend/Routers/UsersRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const usersControllers = require('../controllers/userControllers');
 
 router.get('/api/users', usersControllers.getusers);
-router.post('/api/users', usersControllers.postusers);
+router.post('/api/signup', usersControllers.postusers);
 router.delete('/api/users/:id', usersControllers.deleteusers);
 router.put('/api/users/:id', usersControllers.putusers);
 module.exports = router;
